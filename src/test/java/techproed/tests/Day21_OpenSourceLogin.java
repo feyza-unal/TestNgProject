34ab53d1325eec599fe395a46e92f51e75ba9fb9
feyza-unal <feyzaunal95@gmail.com>
2023-01-27 15:14:24 +0300

    testng

--- src/test/java/techproed/tests/Day21_OpenSourceLogin.java
+++ src/test/java/techproed/tests/Day21_OpenSourceLogin.java
@@ -1,13 +1,15 @@
 package techproed.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import techproed.pages.OpenSourceDashboardPage;
 import techproed.pages.OpenSourcePage;
 import techproed.utilities.ConfigReader;
 import techproed.utilities.Driver;
 
 public class Day21_OpenSourceLogin {
     @Test
-    public void openSourceLogin(){
+    public void openSourceLogin() throws InterruptedException {
 
 //        1.Sayfaya git
         Driver.getDriver().get(ConfigReader.getProperty("open_source_url"));
@@ -16,8 +18,15 @@ public class Day21_OpenSourceLogin {
         OpenSourcePage openSourcePage = new OpenSourcePage();
 
 //        3.Bu obj kullanarak o classdaki objelere ulas
-        openSourcePage.username.sendKeys("Admin");
-        openSourcePage.password.sendKeys("admin123");
+        openSourcePage.username.sendKeys(ConfigReader.getProperty("open_source_username")); //openSourcePage.username.sendKeys("Admin");
+        openSourcePage.password.sendKeys(ConfigReader.getProperty("open_source_password")); //openSourcePage.password.sendKeys("admin123");
         openSourcePage.submitButton.click();
+
+//        4. Assertion
+        OpenSourceDashboardPage openSourceDashboardPage = new OpenSourceDashboardPage();
+        Thread.sleep(10);
+        Assert.assertTrue(openSourceDashboardPage.dashboardHeader.isDisplayed());
+
+
     }
 }
